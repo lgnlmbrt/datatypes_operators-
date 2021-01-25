5d285bb2a2d98f62da8c10c73b5e0d3636bb3aae
Logan Lambert <loganlambert@Logans-MacBook-Air.local>
2021-01-25 15:11:58 -0800

    completed exercise-05

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -1,6 +1,8 @@
 package labs_examples.fundamentals.labs;
 
 
+import java.util.Locale;
+
 /**
  * Fundamentals Exercise 5: Working with Strings
  *
@@ -14,14 +16,25 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+            int x = str.length();
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean equalStrings = str.equalsIgnoreCase(str2);
+        System.out.println("Do str & str2 match? " + equalStrings);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String newString = str + str2;
+        System.out.println(newString);
+
+        System.out.println("Capitalized:" + str.toUpperCase());
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+
+
+
+
 
     }
 
